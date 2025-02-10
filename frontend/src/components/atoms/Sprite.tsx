d79e832b9bc6a45c39878a7d4269f358e0684586
Enrico Weißenrieder <98795399+WE-Kaito@users.noreply.github.com>
2025-02-10 15:27:37 +0100

    basic setup

---
+++ frontend/src/components/atoms/Sprite.tsx
@@ -0,0 +1,6 @@
+const imgSrc =
+  "https://64.media.tumblr.com/437aa40da752cbdc55b000630ab0e387/tumblr_pc891wGELY1uon9hao1_250.gifv";
+
+export default function Sprite() {
+  return <img src={imgSrc} alt={"vpet"} />;
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/atoms/Layout.tsx";
+import Sprite from "../components/atoms/Sprite.tsx";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Sprite />
+    </Layout>
+  );
+}
